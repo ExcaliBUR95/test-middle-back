9a3f7e3a96540ab166c59728fb04bbd395b9baa3
brz95 <abuizidov@bk.ru>
2022-08-29 16:17:08 +0300

    fix code

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/user', require('./routes'));
-app.use('/upload', express.static(path.resolve(__dirname, 'upload')));
+app.use(require("./routes/user"));
+app.use("/upload", express.static(path.resolve(__dirname, "upload")));
 
-const { MONGO_URL, PORT } = process.env;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('Успешное соединение...'))
+  .catch(() => console.log('Пал хила'));
 
-const start = async () => {
-  try {
-    mongoose
-    .connect(MONGO_URL)
-    .then(() => console.log('Успешное соединение...'))
-    .catch(() => console.log('Пал хила'));
-    app.listen(PORT || 7000, () => {
-      console.log('server started on port' + PORT);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-start();
+app.listen(process.env.PORT, () => {
+  console.log('Сервер запущен на http://localhost:8000/');
+});
