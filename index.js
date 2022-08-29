1beac4459ea0026037f76f50676e94dd25f6cfce
brz95 <abuizidov@bk.ru>
2022-08-29 15:44:31 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path");
-require("dotenv").config();
-const { MONGO_URL, PORT } = process.env;
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+
+require('dotenv').config();
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/user", require("./routes/user"));
-app.use("/upload", express.static(path.resolve(__dirname, "upload")));
+app.use('/user', require('./routes'));
+app.use('/upload', express.static(path.resolve(__dirname, 'upload')));
+
+const { MONGO_URL, PORT } = process.env;
 
 const start = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    mongoose
+    .connect(MONGO_URL)
+    .then(() => console.log('Успешное соединение...'))
+    .catch(() => console.log('Пал хила'));
     app.listen(PORT || 7000, () => {
-      console.log("server started on port" + PORT);
+      console.log('server started on port' + PORT);
     });
   } catch (e) {
     console.log(e);
--- middleware/file.middleware.js
+++ middleware/file.middleware.js
@@ -1,27 +1,25 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "upload/");
+    cb(null, 'upload/');
   },
   filename(req, file, cb) {
-    let ext =  req.user._id + path.extname(file.originalname);
-    cb(null,  ext);
+    let ext = req.user._id + path.extname(file.originalname);
+    cb(null, ext);
   },
 });
 
-const types = ["image/jpg", "image/jpeg", "image/png"];
-
+const types = ['image/jpg', 'image/jpeg', 'image/png'];
 
 const fileFilter = (req, file, cb) => {
-  const fileSize = parseInt(req.headers["content-length"])
+  const fileSize = parseInt(req.headers['content-length']);
   if (types.includes(file.mimetype) && fileSize <= 1282810) {
     cb(null, true);
   } else {
     cb(null, false);
   }
-
 };
 
 module.exports = multer({ storage, fileFilter });
